10de04cae69d953293c5c989f3c63981c558850d
DanDok20 <85905111+DanDok20@users.noreply.github.com>
2023-09-11 11:28:12 -0500

    Add files via upload

---
+++ Codigos_Repaso_C/Hola_mundo.c
@@ -0,0 +1,17 @@
+/*
+
+// C++
+#include <iostream>
+
+int main(){
+  std::cout << "Hello World!!!" << std::endl;
+}
+
+*/
+
+// C
+#include "stdio.h"
+
+int main(){
+  printf("Hello Wolrd!!!\n");
+}
